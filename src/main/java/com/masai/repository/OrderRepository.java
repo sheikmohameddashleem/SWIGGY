4e4093a22292d32b4ad0c519560b2699c237b729
sheik kalibullah <121329816+sheikmohameddashleem@users.noreply.github.com>
2023-07-25 23:58:38 +0530

    D___S

--- src/main/java/com/masai/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.masai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.masai.model.Orders;
-
-public interface OrderRepository extends JpaRepository<Orders, Integer> {
-
-}
